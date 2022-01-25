dd2a388faf8d8f75f56ba912556bcc5d3a565ae3
enter-opy <vivekvijayan6282@gmail.com>
2022-01-25 20:05:27 +0530

    Update frequency.java

--- exp 2/frequency.java
+++ exp 2/frequency.java
@@ -8,12 +8,12 @@ public class frequency {
         String str = obj.nextLine();
 
         System.out.println("Enter a character to count the frequency: ");
-        char ch = obj.next().charAt(0).toLowerCase();
+        char ch = Character.toLowerCase(obj.next().charAt(0));
 
         int count = 0;
 
         for (int i = str.length() - 1; i >= 0; i--) {
-            if (str.charAt(i).toLowerCase() == ch) {
+            if (Character.toLowerCase(str.charAt(i)) == ch) {
                 count += 1;
             }
         }
